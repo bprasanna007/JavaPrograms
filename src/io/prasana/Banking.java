813a2e16a5cacc769db3a109834b0484e883e19d
PRASANNA B <bprasanna.study@gmail.com>
2026-02-12 21:54:05 +0530

    Add files via upload

--- src/io/prasana/Banking.java
+++ src/io/prasana/Banking.java
@@ -2,7 +2,9 @@ interface Payment {
     void pay(double amount);
 }
 
+// ABSTRACT CLASS
 abstract class Account {
+
     private double balance;
 
     public void setBalance(double balance) {
@@ -16,63 +18,94 @@ abstract class Account {
     abstract void calculateInterest();
 
     public void deposit(double amount) {
+        if (amount <= 0) {
+            System.out.println("Invalid deposit amount");
+            return;
+        }
+
         balance += amount;
-        System.out.println("Deposited " + amount);
+        System.out.println("Deposited: " + amount);
+    }
+
+    public void withdraw(double amount) {
+
+        if (amount <= 0) {
+            System.out.println("Invalid withdrawal amount");
+        }
+        else if (amount > balance) {
+            System.out.println("Insufficient balance");
+        }
+        else {
+            balance -= amount;
+            System.out.println("Withdrawal successful: " + amount);
+            System.out.println("Remaining balance: " + balance);
+        }
     }
 }
 
+// SAVINGS ACCOUNT
 class SavingsAccount extends Account {
+
+    @Override
     void calculateInterest() {
         double interest = getBalance() * 0.05;
-        System.out.println("Savings " + interest);
+        System.out.println("Savings Interest: " + interest);
     }
 }
 
+// CURRENT ACCOUNT
 class CurrentAccount extends Account {
+
+    @Override
     void calculateInterest() {
-        double interest = getBalance() * 0.1;
-        System.out.println("Savings " + interest);
-
+        double interest = getBalance() * 0.10;
+        System.out.println("Current Interest: " + interest);
     }
 }
 
-class upi implements Payment {
+// UPI PAYMENT
+class UPI implements Payment {
+
+    @Override
     public void pay(double amount) {
-        System.out.println("Paying " + amount);
-
+        System.out.println("Paid using UPI: " + amount);
     }
-
-
 }
 
-class credit implements Payment {
+// CREDIT CARD PAYMENT
+class Credit implements Payment {
+
+    @Override
     public void pay(double amount) {
-        System.out.println("Credit " + amount);
+        System.out.println("Paid using Credit Card: " + amount);
     }
 }
 
-
+// MAIN CLASS
 public class Banking {
+
     public static void main(String[] args) {
+
         Account acc;
+
         acc = new SavingsAccount();
         acc.setBalance(100000);
-
         acc.deposit(2000);
+        acc.withdraw(5000);
         acc.calculateInterest();
 
         acc = new CurrentAccount();
         acc.setBalance(10000);
-
         acc.deposit(3000);
+        acc.withdraw(15000); // insufficient
         acc.calculateInterest();
 
-
         Payment p;
-        p = new upi();
-        p.pay(500);
-        p = new credit();
+
+        p = new UPI();
         p.pay(500);
 
+        p = new Credit();
+        p.pay(1000);
     }
 }
