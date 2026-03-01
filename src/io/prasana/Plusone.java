7847aafcdb0b91ef86a97a81d04207d645f50125
PRASANNA B <bprasanna.study@gmail.com>
2026-03-01 22:11:44 +0530

    Add files via upload

---
+++ src/io/prasana/Plusone.java
@@ -0,0 +1,35 @@
+public class Plusone {
+
+    public static int[] plusOne(int[] digits) {
+
+        
+        for (int i = digits.length - 1; i >= 0; i--) {
+
+
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+
+            digits[i] = 0;
+        }
+
+
+        int[] newArray = new int[digits.length + 1];
+        newArray[0] = 1;
+
+        return newArray;
+    }
+
+    public static void main(String[] args) {
+
+        int[] digits = {9, 9, 9};
+
+        int[] result = plusOne(digits);
+
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
