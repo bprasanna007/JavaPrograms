fa84ad7eae8e206eac210a7d8bdf35b1ca598541
PRASANNA B <bprasanna.study@gmail.com>
2026-03-05 20:41:00 +0530

    Add files via upload

---
+++ src/io/prasana/SecondlargestelementinArray.java
@@ -0,0 +1,23 @@
+public class SecondlargestelementinArray{
+    public static void main(String[] args) {
+
+        int[] arr = {10, 2, 50, 80, 900};
+
+        int largest = arr[0];
+        int secondLargest = arr[0];
+
+        for(int i = 1; i < arr.length; i++) {
+
+            if(arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            }
+
+            else if(arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+
+        System.out.println("Second Largest = " + secondLargest);
+    }
+}
