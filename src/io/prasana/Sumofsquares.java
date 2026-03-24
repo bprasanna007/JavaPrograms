ee1489bfc5631de882ccbf1d29ff4f14a6495f11
PRASANNA B <bprasanna.study@gmail.com>
2026-03-24 20:51:00 +0530

    Add files via upload

---
+++ src/io/prasana/Sumofsquares.java
@@ -0,0 +1,11 @@
+public class Sumofsquares {
+    public static void main(String[] args) {
+        int sum=0;
+        int n=8;
+        for(int i=0;i<=n;i++){
+            sum=sum+(i*i);
+
+        }
+        System.out.println(sum);
+    }
+}
