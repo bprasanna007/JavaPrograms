c6f67d49f4af6580904c38bbe652b3f7850ced05
PRASANNA B <bprasanna.study@gmail.com>
2026-03-24 20:55:27 +0530

    Add files via upload

---
+++ src/io/prasana/Swapnumbers.java
@@ -0,0 +1,12 @@
+public class Swapnumbers {
+    public static void main(String[] args) {
+        int a=2;
+        int b=3;
+        int temp;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
